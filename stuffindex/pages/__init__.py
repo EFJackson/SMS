52b61d2ec8b7f179f9049acf1b73217485e48676
EFJackson <euanfjackson@gmail.com>
2013-08-07 14:41:55 +0100

    A raft of changes, mostly breaking stuff. Also a a start on the database.

--- stuffindex/pages/__init__.py
+++ stuffindex/pages/__init__.py
@@ -6,8 +6,6 @@ from webapp2_extras import sessions
 from mako import lookup
 import plim
 
-import facebook
-
 from stuffindex import config
 
 mako_render = lookup.TemplateLookup(directories=['stuffindex/pages/templates'],
@@ -18,20 +16,14 @@ class BaseHandler(webapp2.RequestHandler):
     def dispatch(self):
         # Get a session store for this request.
         self.session_store = sessions.get_store(request=self.request)
-        self.user = facebook.get_user_from_cookie(self.request.cookies,
-                                                  config.facebook_app_key,
-                                                  config.facebook_app_secret)
         self.render_globals = {'uri_for': webapp2.uri_for,
                                'current_url': self.request.url,
                                'current_page': self.request.path_qs,
                                'request': self.request,
-                               'facebook': facebook,
                                'user': self.user,
                                'pretty': pprint.pformat,
                                }
-        if self.user is not None:
-            graph = facebook.GraphAPI(self.user["access_token"])
-            self.profile = graph.get_object("me")
+        if self.user is not None:            self.profile = graph.get_object("me")
             self.render_globals['profile'] = self.profile
 
         try:
