2bc388173bc965ccbb461137a5bc3ded3b7581b7
CamLewWri <cameronwright02@gmail.com>
2013-08-08 15:58:17 +0100

    Updated javascript and html

---
+++ stuffindex/pages/templates/mapsinit.js
@@ -0,0 +1,12 @@
+		function initialize()
+		{
+			var mapProp = {
+			center:new google.maps.LatLng(51.508742,-0.120850),
+			zoom:6,
+			mapTypeId:google.maps.MapTypeId.ROADMAP
+		};
+		var map=new google.maps.Map(document.getElementById("googleMap")
+		,mapProp);
+		}
+
+		google.maps.event.addDomListener(window, 'load', initialize);
