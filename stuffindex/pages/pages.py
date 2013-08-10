7696e7f159cdb58ac57733c00ae40f49fc373694
CamLewWri <cameronwright02@gmail.com>
2013-08-10 09:24:21 +0100

    Started category menu

--- stuffindex/pages/pages.py
+++ stuffindex/pages/pages.py
@@ -1,36 +1,42 @@
 from webapp2 import uri_for
 
+import time
+
+import gridfs
+
 from stuffindex.pages import BaseHandler
 from stuffindex.application import application as app
 
 from stuffindex.utils import redirect, render
 
-from stuffindex.models.stuff import stuff
+from stuffindex.models.stuff import Stuff
+from stuffindex.models import gfs
 
 
 @app.route('/', 'index')
 class Index(BaseHandler):
     def get(self):
-        return self.render('index.html', stuffes=stuff.get_all())
+        return self.render('index.html', stuffes=Stuff.get_all())
 
 
 @app.route('/add_stuff', 'add_stuff')
-class Addstuff(BaseHandler):
-    def get(self):
-        return self.render('add_stuff.slim')
-
+class AddStuff(BaseHandler):
     def post(self):
-        if self.user is not None:
-            uid = self.profile['id']
-            he_said = render(self.request.get('he_said'))
-            it_means = render(self.request.get('it_means'))
-            b = stuff(uid, he_said, it_means)
-            b.push()
-        raise redirect(uri_for('index'))
+        print self.request.POST
+        x_coord = self.request.POST.get('x_coord', 0)
+        y_coord = self.request.POST.get('y_coord', 0)
+        category = self.request.POST.get('category', 'Other')
+        date = self.request.POST.get('date', str(int(time.time())))
+        img = self.request.POST.get('img', None).value
+        image_file_id = gfs.put(img)
+        print image_file_id
+        stuff = Stuff(image_file_id, (x_coord, y_coord), date, category)
+        print stuff.push()
 
-
-@app.route('/stuff/<stuff_id>', 'view_stuff')
-class Viewstuff(BaseHandler):
+@app.route('/img/<stuff_id>', 'view_image')
+class ViewImage(BaseHandler):
     def get(self, stuff_id):
-        b = stuff.get_by_id(stuff_id)
-        return self.render('view_stuff.slim', stuff=b)
+        stuff = Stuff.get_by_id(stuff_id)
+        img = gfs.get(stuff.image).read()
+        self.response.content_type = "image/jpg"
+        self.response.write(img)
