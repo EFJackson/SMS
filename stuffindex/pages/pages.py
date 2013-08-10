a268ec8154c80f9a130e1071476cf3975d7ae125
Kit Barnes <kit@ninjalith.com>
2013-08-10 11:46:27 +0200

    Add a "list all" ability

--- stuffindex/pages/pages.py
+++ stuffindex/pages/pages.py
@@ -45,9 +45,13 @@ class ViewImage(BaseHandler):
 @app.route('/list_stuff', 'list_stuff')
 class ListStuff(BaseHandler):
     def get(self):
-        x_coord = self.request.GET.get('x_coord', 0)
-        y_coord = self.request.GET.get('y_coord', 0)
-        stuffes = Stuff.get_near((x_coord, y_coord), 100)
+        all_stuff = self.request.GET.get('all', False)
+        if all_stuff:
+            stuffes = Stuff.get_all()
+        else:
+            x_coord = self.request.GET.get('x_coord', 0)
+            y_coord = self.request.GET.get('y_coord', 0)
+            stuffes = Stuff.get_near((x_coord, y_coord), 100)
         return self.render('list_stuff.html', stuffes=stuffes)
 
 
